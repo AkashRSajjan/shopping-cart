5d63b76575a56798c10d77ea4977bb4a40dfc49f
AkashRSajjan <akashrsajjan@gmail.com>
2019-09-07 19:13:31 +0530

    Updates

---
+++ precache-manifest.49b19f67c196dd239a1218b170111b59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c10ccca9cb7d3177de906dd9fae0d9",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "c21845ef1e14f18401fc",
+    "url": "/shopping-cart/static/css/main.26dcbe8e.chunk.css"
+  },
+  {
+    "revision": "405293bf6792ce6ac0a8",
+    "url": "/shopping-cart/static/js/2.34e79248.chunk.js"
+  },
+  {
+    "revision": "c21845ef1e14f18401fc",
+    "url": "/shopping-cart/static/js/main.99dfbde8.chunk.js"
+  },
+  {
+    "revision": "7b8061fd9141bb11e4da",
+    "url": "/shopping-cart/static/js/runtime~main.794fc87a.js"
+  }
+]);
